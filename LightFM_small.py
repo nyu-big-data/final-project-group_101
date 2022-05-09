099a866e391ab49c3b3667dd9bc87ddde9c999ca
Vanessa Xu <zx657@hlog-2.hpc.nyu.edu>
2022-05-09 16:26:55 -0400

    updated LightFM_small.py

--- LightFM_small.py
+++ LightFM_small.py
@@ -16,41 +16,47 @@ print('Final Project Extension LightFM on Small dataset')
 netID = getpass.getuser()
 
 print('Reading ratings.csv and specifying schema')
-small_train_path = "hdfs:/user/" + netID + "/ratings_small_train.csv"
-small_val_path = "hdfs:/user/" + netID + "/ratings_small_val.csv"
-small_test_path = "hdfs:/user/" + netID + "/ratings_small_test.csv"
+small_train_path = "/home/" + netID + "/ratings_small_train.csv"
+small_val_path = "/home/" + netID + "/ratings_small_val.csv"
+small_test_path = "/home/" + netID + "/ratings_small_test.csv"
 
 # Load dataset fro HDFS
 ratings_small_train = pd.read_csv(small_train_path, names = ('userId', 'movieId', 'rating', 'timestamp'))
 ratings_small_val = pd.read_csv(small_val_path, names = ('userId', 'movieId', 'rating', 'timestamp', "median_timestamp"))
 ratings_small_test = pd.read_csv(small_test_path, names = ('userId', 'movieId', 'rating', 'timestamp', "median_timestamp"))
 
-# # drop median_timestamp & timestamp columns
+print('Dropping irrelavant columns')
+# drop median_timestamp & timestamp columns
 ratings_small_val = ratings_small_val.drop(columns=['median_timestamp'])
 ratings_small_test = ratings_small_test.drop(columns=['median_timestamp'])
 ratings_small_train = ratings_small_train.drop(columns=['timestamp'])
 ratings_small_val = ratings_small_val.drop(columns=['timestamp'])
 ratings_small_test = ratings_small_test.drop(columns=['timestamp'])
 
+print('Reassigning movie IDs to avoid dimension error')
 # reassign movie IDs to avoid dimension error when fitting LightFM model
 total_item_user = pd.concat([ratings_small_train, ratings_small_test, ratings_small_val]).drop_duplicates()
 total_item_user = total_item_user.sort_values(['movieId'])
 total_item_user['new_movieId'] = (total_item_user.groupby(['movieId'], sort=False).ngroup()+1)
 
+print('Appending new_movieId to existing train, test, and validation sets')
 # append new_movieId to existing train, test, and validation test sets
 ratings_small_train = ratings_small_train.merge(total_item_user, on=['movieId','userId','rating'], how="left")
 ratings_small_val = ratings_small_val.merge(total_item_user, on=['movieId','userId','rating'], how="left")
 ratings_small_test = ratings_small_test.merge(total_item_user, on=['movieId','userId','rating'], how="left")
 
+print('Dropping original movieId columns')
 # drop original movieId column
 ratings_small_train = ratings_small_train.drop(columns=['movieId'])
 ratings_small_val = ratings_small_val.drop(columns=['movieId'])
 ratings_small_test = ratings_small_test.drop(columns=['movieId'])
 
+print("Adjusting dataset dimensions to avoid unmatched dimension error")
 # adjust dataset dimensions to avoid error of unmatched dimensions
 data = Dataset()
 data.fit(users = np.unique(total_item_user["userId"]), items = np.unique(total_item_user["new_movieId"]))
 
+print("building interactions")
 # build interactions
 interactions_train, weights_train = data.build_interactions([(ratings_small_train['userId'][i], 
                                                               ratings_small_train['new_movieId'][i],
@@ -60,6 +66,7 @@ interactions_val, weights_val = data.build_interactions([(ratings_small_val['use
                                                           ratings_small_val['rating'][i]) for i in range(ratings_small_val.shape[0])])
 
 # WARP model
+print("start training WARP model")
 t = time()
 model = LightFM(loss='warp', learning_rate=0.05)
 model = model.fit(interactions = interactions_train, sample_weight= weights_train, 
@@ -72,6 +79,7 @@ print("Precision at k is:", val_precision)
 print("Time spent is:", t_round)
 
 # BPR model
+print("start training BPR model")
 t = time()
 model = LightFM(loss='bpr', learning_rate=0.05)
 model = model.fit(interactions = interactions_train, sample_weight= weights_train, 
