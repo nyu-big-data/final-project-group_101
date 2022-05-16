992407b493f06f995bb18ebc628aa0a69a552653
ruizheng0521 <47020986+ruizheng0521@users.noreply.github.com>
2022-05-16 14:25:14 -0400

    Update annoy_extension.py

--- CheckPoint Submission/annoy_extension.py
+++ CheckPoint Submission/annoy_extension.py
@@ -23,7 +23,7 @@ import time
 import numpy as np
 from tqdm import tqdm_notebook as tqdm 
 import matplotlib.pyplot as plt 
-pip-install annoy
+#pip-install annoy
 from annoy import AnnoyIndex
 #pip install pyarrow
 
