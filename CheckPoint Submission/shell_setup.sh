c11682e4cad56311aa37825fbe786ad1065637fa
ruizheng0521 <47020986+ruizheng0521@users.noreply.github.com>
2022-05-16 05:16:58 -0400

    Update shell_setup.sh

--- CheckPoint Submission/shell_setup.sh
+++ CheckPoint Submission/shell_setup.sh
@@ -3,10 +3,6 @@ export HADOOP_EXE='/usr/bin/hadoop'
 module load python/gcc/3.7.9
 module load spark/3.0.1
 
-pip install pyarrow
-module load pyarrow
-
-
 
 alias hfs="$HADOOP_EXE fs"
 alias spark-submit='PYSPARK_PYTHON=$(which python) spark-submit'
